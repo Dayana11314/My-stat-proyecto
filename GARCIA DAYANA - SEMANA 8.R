fbc488e9b535ddc83e0a64af8cb5cc2cac1d54fc
Dayana Garcia <dayanai.garcia@upsjb.edu.pe>
2025-12-07 17:38:56 -0500

    Add files via upload

---
+++ GARCIA DAYANA - SEMANA 8.R
@@ -0,0 +1,8 @@
+CARGANDO LA DATA A TRABAJAR 
+
+library(readr)
+s03_treat_artritis_5_ <- read_csv("C:/Users/DAYANA/Downloads/s03_treat_artritis (5).csv")
+View(s03_treat_artritis_5_)
+
+Install.packages("readr") 
+Library(readr)
